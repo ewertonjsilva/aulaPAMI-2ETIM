c5752484d52677ac0d7a6a8009ff54fd25e18d8b
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-10-29 15:15:10 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native'; 
+
+// npm i expo-constants
+import Constants from 'expo-constants';
+
+// npm i react-native-responsive-fontsize
+
+import Exemplo1 from './src/exemplos/ex_01'; 
+import Atividade1 from './src/atividades/atv_01';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Atividade1 />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#aaa',
+    padding: 16, 
+    paddingTop: Constants.statusBarHeight || 16,     
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
